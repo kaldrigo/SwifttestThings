7c6e73fe9e411e6c43e950862a520c29211e1fe3
Bruno <1brunobms@gmail.com>
2019-09-23 12:00:03 -0300

    Implementando da tela de levels

---
+++ SwifttestThings/Model/Activity.swift
@@ -0,0 +1,21 @@
+//
+//  Atividade.swift
+//  SwifttestThings
+//
+//  Created by Bruno Melo da Silva on 23/09/19.
+//  Copyright © 2019 Rodrigo Azeredo. All rights reserved.
+//
+
+import Foundation
+
+struct Activity {
+    var section: String = ""
+    var name: String = ""
+    var description: String = ""
+    var examplo: String = ""
+    var exercise: String = ""
+    var responseUser: String = ""
+    var response: String = ""
+    var codeOptions: [String] = [String]()
+    var moreInfo: String = ""
+}

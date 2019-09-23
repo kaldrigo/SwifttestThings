bd5b11079ddbf865a7c5e77f3d488ecb9e5b0682
Rodrigo Azeredo <rodrigoazeredo_@hotmail.com>
2019-09-23 10:47:41 -0300

    Adicionado model Activity

---
+++ SwifttestThings/Model/Activity.swift
@@ -0,0 +1,21 @@
+//
+//  Activity.swift
+//  SwifttestThings
+//
+//  Created by Rodrigo Azeredo on 23/09/19.
+//  Copyright © 2019 Rodrigo Azeredo. All rights reserved.
+//
+
+import Foundation
+
+struct Activity {
+    var section: String = ""
+    var name: String = ""
+    var description: String = ""
+    var examplo: String = ""
+    var exercise: String = ""
+    var responseUser: String = ""
+    var response: String = ""
+    var codeOptions: [String] = [String]()
+    var moreInfo: String = ""
+}
